55935300acfffa33c4ac1a041e559ab4513f4177
lbavendanos <lbavendanos@gmail.com>
2021-02-19 13:31:59 -0500

    Fix api

--- lib/api/movies_api.dart
+++ lib/api/movies_api.dart
@@ -1,11 +1,18 @@
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:discover_movie/models/response.dart';
 import 'package:discover_movie/models/movie.dart';
 
 Future<Response<Movie>> fetchPopularMovies() async {
-  final Map<String, String> queryParameters = {};
-  final uri =
-      Uri.https('api.themoviedb.org', '3/movie/popular', queryParameters);
+  String apiKey = env['API_KEY'];
+  String apiDomain = env['API_DOMAIN'];
+
+  final Map<String, String> queryParameters = {
+    'api_key': apiKey,
+    'language': 'en-US',
+    'page': '1',
+  };
+  final uri = Uri.https(apiDomain, '3/movie/popular', queryParameters);
 
   final response = await http.get(uri);
 
